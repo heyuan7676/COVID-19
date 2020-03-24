608a39f81c4244f578c5b589da7b36600b91a5f8
Yuan He <yuanhe777tt@hotmail.com>
2020-03-24 17:17:47 -0400

    modified download_data

--- download_data.sh
+++ download_data.sh
@@ -21,9 +21,6 @@ wget https://storage.googleapis.com/gtex_analysis_v8/rna_seq_data/GTEx_Analysis_
 gunzip  GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct.gz
 TPM="GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct"
 
-#Extract just the genes we are interested in.
-grep -m1 "Description" $TPM > gene_tpm.gct ; sed -n 53469p $TPM >> gene_tpm.gct ; sed -n 51843p $TPM >> gene_tpm.gct
-
 #Subject phenotypes:
 wget https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SubjectPhenotypesDS.txt
 #Sample annotations:
